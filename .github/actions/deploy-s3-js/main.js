7fe69a8313138fed9f94827b92fa8b41002fbdbf
manuelramirez <manuel.ramirez@xebia.com>
2024-11-07 13:38:23 -0500

    js action with creds

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -4,7 +4,16 @@ const exec = require('@actions/exec')
 
 
 function run (){
-    core.notice('Hello from my custom js action')
+    core.notice('Hello from my custom js action');
+
+    // get inputs
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    const s3Uri = `s3://${bucket}`
+    // upload files
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 }
 
 run();
